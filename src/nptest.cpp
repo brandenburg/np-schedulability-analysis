1e0ce832e55e01cbf2cbac85927d6410ae3d24f7
Björn Brandenburg <bbb@mpi-sws.org>
2017-09-06 22:08:37 +0200

    nptest: support naive analysis and the storing of schedule graphs

--- src/nptest.cpp
+++ src/nptest.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
-
-
 
 #include "OptionParser.h"
 
@@ -9,41 +8,84 @@
 #include "io.hpp"
 #include "clock.hpp"
 
+// command line options
+static bool want_naive;
+static bool want_dense;
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+static bool want_dot_graph;
+#endif
+
+
 struct Analysis_result {
 	bool schedulable;
 	unsigned long number_of_states;
 	double cpu_time;
+	std::string graph;
 };
 
-static Analysis_result process_stream(std::istream &in, bool want_dense)
+static Analysis_result process_stream(std::istream &in)
 {
 	if (want_dense) {
 		auto c = Processor_clock();
 		auto jobs = NP::parse_file<dense_t>(in);
+
 		c.start();
-		auto space = NP::Uniproc::State_space<dense_t>::explore(jobs);
+		auto space = want_naive ?
+			NP::Uniproc::State_space<dense_t>::explore_naively(jobs):
+			NP::Uniproc::State_space<dense_t>::explore(jobs);
 		c.stop();
-		return {space.is_schedulable(), space.number_of_states(), c};
+
+		auto graph = std::ostringstream();
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+		if (want_dot_graph)
+			graph << space;
+#endif
+
+		return {space.is_schedulable(), space.number_of_states(), c, graph.str()};
+
 	} else {
+
 		auto c = Processor_clock();
 		auto jobs = NP::parse_file<dtime_t>(in);
+
 		c.start();
-		auto space = NP::Uniproc::State_space<dtime_t>::explore(jobs);
+		auto space = want_naive ?
+			NP::Uniproc::State_space<dtime_t>::explore_naively(jobs):
+			NP::Uniproc::State_space<dtime_t>::explore(jobs);
 		c.stop();
-		return {space.is_schedulable(), space.number_of_states(), c};
+
+		auto graph = std::ostringstream();
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+		if (want_dot_graph)
+			graph << space;
+#endif
+
+		return {space.is_schedulable(), space.number_of_states(), c, graph.str()};
 	}
 }
 
-static void process_file(const std::string& fname, bool want_dense)
+static void process_file(const std::string& fname)
 {
 	try {
 		Analysis_result result;
 
 		if (fname == "-")
-			result = process_stream(std::cin, want_dense);
+			result = process_stream(std::cin);
 		else {
 			auto in = std::ifstream(fname, std::ios::in);
-			result = process_stream(in, want_dense);
+			result = process_stream(in);
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+			if (want_dot_graph) {
+				std::string dot_name = fname;
+				auto p = dot_name.find(".csv");
+				if (p != std::string::npos) {
+					dot_name.replace(p, std::string::npos, ".dot");
+					auto out  = std::ofstream(dot_name,  std::ios::out);
+					out << result.graph;
+					out.close();
+				}
+			}
+#endif
 		}
 
 		std::cout << fname
@@ -67,16 +109,30 @@ int main(int argc, char** argv)
 	      .metavar("TIME-MODEL").choices({"dense", "discrete"})
 	      .help("choose 'discrete' or 'dense' time (default: discrete)");
 
+	parser.add_option("-n", "--naive").dest("naive").set_default("0")
+	      .action("store_const").set_const("1")
+	      .help("use the naive exploration method (default: merging)");
+
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+	parser.add_option("-g", "--save-graph").dest("dot").set_default("0")
+	      .action("store_const").set_const("1")
+	      .help("store the state graph in Graphviz dot format (default: off)");
+#endif
+
 	auto options = parser.parse_args(argc, argv);
 
 	const std::string& time_model = options.get("time_model");
-	bool use_dense_time = time_model == "dense";
+	want_dense = time_model == "dense";
+	want_naive = options.get("naive");
+#ifdef CONFIG_COLLECT_SCHEDULE_GRAPH
+	want_dot_graph = options.get("dot");
+#endif
 
 	for (auto f : parser.args())
-		process_file(f, use_dense_time);
+		process_file(f);
 
 	if (parser.args().empty())
-		process_file("-", use_dense_time);
+		process_file("-");
 
 	return 0;
 }
