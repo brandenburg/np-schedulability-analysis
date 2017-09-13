c0d4aa4ef5a33dd1915ac3a5843b2e47281dc75e
Björn Brandenburg <bbb@mpi-sws.org>
2017-09-13 12:30:04 +0200

    fix split.sh script

--- scripts/split.sh
+++ scripts/split.sh
@@ -4,7 +4,8 @@ WORKLOAD=$1
 
 NAME=`basename $WORKLOAD`
 COUNT=`wc -l $WORKLOAD | awk '{print $1}'`
-for l in $(seq -w 2  932)
-do
-    head -n $l $WORKLOAD > ${NAME/.csv/}_split=$l.csv
+for l in $(seq -w 2  $COUNT)
+do 
+    echo $WORKLOAD '->' ${NAME/.csv/}_split=${l}.csv
+    head -n $l $WORKLOAD > ${NAME/.csv/}_split=${l}.csv
 done
