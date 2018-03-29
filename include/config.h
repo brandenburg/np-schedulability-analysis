1128a24e96f96a352a3a7b919974bf8634cdc5b9
Björn Brandenburg <bbb@mpi-sws.org>
2018-03-29 17:53:08 +0200

    introduce CONFIG_PARALLEL
    allow CONFIG_PARALLEL only if !CONFIG_COLLECT_SCHEDULE_GRAPH

--- include/config.h
+++ include/config.h
@@ -5,7 +5,14 @@
 // #define DM(x) std::cerr << x
 #define DM(x)
 
-
 // #define CONFIG_COLLECT_SCHEDULE_GRAPH
+
+#ifndef CONFIG_COLLECT_SCHEDULE_GRAPH
+#define CONFIG_PARALLEL
+#endif
+
+#ifndef NDEBUG
+#define TBB_USE_DEBUG 1
+#endif
 
 #endif
