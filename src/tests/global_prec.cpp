fad63bde84e4964b810199da9d65b3b6e5230bab
Björn Brandenburg <bbb@mpi-sws.org>
2018-05-24 01:08:37 +0200

    add tests for the global DAG analysis

---
+++ src/tests/global_prec.cpp
@@ -0,0 +1,135 @@
+#include "doctest.h"
+
+#include <iostream>
+#include <sstream>
+
+#include "io.hpp"
+#include "global/space.hpp"
+
+const std::string ts1_jobs =
+"Task ID, Job ID, Arrival min, Arrival max, Cost min, Cost max, Deadline, Priority\n"
+"      1,      1,           0,        6000,     5000,     9000,    30000,    30000\n"
+"      1,      2,           0,        6000,     3000,     6000,    30000,    30000\n"
+"      1,      3,           0,        6000,     2000,    15000,    30000,    30000\n"
+"      2,      1,           0,        3000,     5000,    10000,    30000,    30000\n"
+"      2,      2,           0,        3000,     3000,     5000,    30000,    30000\n";
+
+const std::string ts1_edges =
+"From TID, From JID,   To TID,   To JID\n"
+"       1,        1,        1,        2\n"
+"       1,        1,        1,        3\n"
+"       2,        1,        2,        2\n";
+
+const std::string ts2_jobs =
+"Task ID, Job ID, Arrival min, Arrival max, Cost min, Cost max, Deadline, Priority\n"
+"      1,      1,           0,           0,     2000,     5000,    40000,    40000\n"
+"      1,      2,           0,           0,     3000,    10000,    40000,    40000\n"
+"      1,      3,           0,           0,     3000,    10000,    40000,    40000\n"
+"      1,      4,           0,           0,     3000,    10000,    40000,    40000\n"
+"      1,      5,           0,           0,     5000,    15000,    40000,    40000\n"
+"      2,      1,           0,       40000,        0,    10000,    80000,    80000\n"
+"      1,     11,       40000,       40000,     2000,     5000,    80000,    80000\n"
+"      1,     12,       40000,       40000,     3000,    10000,    80000,    80000\n"
+"      1,     13,       40000,       40000,     3000,    10000,    80000,    80000\n"
+"      1,     14,       40000,       40000,     3000,    10000,    80000,    80000\n"
+"      1,     15,       40000,       40000,     5000,    15000,    80000,    80000\n";
+
+const std::string ts2_edges =
+"From TID, From JID,   To TID,   To JID\n"
+"       1,        1,        1,        2\n"
+"       1,        1,        1,        3\n"
+"       1,        1,        1,        4\n"
+"       1,        2,        1,        5\n"
+"       1,        3,        1,        5\n"
+"       1,        4,        1,        5\n"
+"       1,       11,        1,       12\n"
+"       1,       11,        1,       13\n"
+"       1,       11,        1,       14\n"
+"       1,       12,        1,       15\n"
+"       1,       13,        1,       15\n"
+"       1,       14,        1,       15\n";
+
+TEST_CASE("[global-prec] taskset-1") {
+	auto dag_in = std::istringstream(ts1_edges);
+	auto dag = NP::parse_dag_file(dag_in);
+
+	auto in = std::istringstream(ts1_jobs);
+	auto jobs = NP::parse_file<dtime_t>(in);
+
+	NP::validate_prec_refs<dtime_t>(dag, jobs);
+
+	auto num_cpus = 2;
+
+	auto nspace2 = NP::Global::State_space<dtime_t>::explore_naively(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK_FALSE(nspace2.is_schedulable());
+
+	auto space2 = NP::Global::State_space<dtime_t>::explore(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK_FALSE(space2.is_schedulable());
+
+	num_cpus = 3;
+
+	auto nspace3 = NP::Global::State_space<dtime_t>::explore_naively(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(nspace3.is_schedulable());
+
+	auto space3 = NP::Global::State_space<dtime_t>::explore(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(space3.is_schedulable());
+
+	for (const NP::Job<dtime_t>& j : jobs) {
+		CHECK(nspace3.get_finish_times(j) == space3.get_finish_times(j));
+		CHECK(nspace3.get_finish_times(j).from() != 0);
+	}
+}
+
+TEST_CASE("[global-prec] taskset-2") {
+	auto dag_in = std::istringstream(ts2_edges);
+	auto dag = NP::parse_dag_file(dag_in);
+
+	auto in = std::istringstream(ts2_jobs);
+	auto jobs = NP::parse_file<dtime_t>(in);
+
+	NP::validate_prec_refs<dtime_t>(dag, jobs);
+
+	auto num_cpus = 2;
+
+	auto nspace2 = NP::Global::State_space<dtime_t>::explore_naively(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(nspace2.is_schedulable());
+
+	auto space2 = NP::Global::State_space<dtime_t>::explore(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(space2.is_schedulable());
+
+	for (const NP::Job<dtime_t>& j : jobs) {
+		CHECK(nspace2.get_finish_times(j) == space2.get_finish_times(j));
+		if (j.least_cost() != 0)
+			CHECK(nspace2.get_finish_times(j).from() != 0);
+	}
+
+	num_cpus = 3;
+
+	auto nspace3 = NP::Global::State_space<dtime_t>::explore_naively(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(nspace3.is_schedulable());
+
+	auto space3 = NP::Global::State_space<dtime_t>::explore(jobs,
+		0, 1000, dag, num_cpus);
+
+	CHECK(space3.is_schedulable());
+
+	for (const NP::Job<dtime_t>& j : jobs) {
+		CHECK(nspace3.get_finish_times(j) == space3.get_finish_times(j));
+		if (j.least_cost() != 0)
+			CHECK(nspace3.get_finish_times(j).from() != 0);
+	}
+}
