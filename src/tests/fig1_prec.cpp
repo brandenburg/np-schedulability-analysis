a37596ec904d5dc762f701905e3be1bf4852ea4d
Björn Brandenburg <bbb@mpi-sws.org>
2019-01-12 12:57:48 +0100

    add tests for malformed DAGs

--- src/tests/fig1_prec.cpp
+++ src/tests/fig1_prec.cpp
@@ -52,3 +52,71 @@ TEST_CASE("[prec] RTSS17-Fig1a") {
 		CHECK(nspace.get_finish_times(j).from() != 0);
 	}
 }
+
+const std::string prec_dag_file_with_cycle =
+"Predecessor TID,	Predecessor JID,	Successor TID, Successor JID\n"
+"1, 1,    1, 2\n"
+"1, 2,    1, 3\n"
+"1, 3,    1, 4\n"
+"1, 4,    1, 5\n"
+"1, 5,    1, 6\n"
+"1, 6,    1, 1\n"  // back edge
+"2, 7,    2, 8\n"
+"1, 2,    3, 9\n";
+
+TEST_CASE("[prec] handle cycles gracefully") {
+	auto dag_in = std::istringstream(prec_dag_file_with_cycle);
+	auto dag = NP::parse_dag_file(dag_in);
+
+	auto in = std::istringstream(fig1a_jobs_file);
+	auto jobs = NP::parse_file<dtime_t>(in);
+
+	NP::validate_prec_refs<dtime_t>(dag, jobs);
+
+	auto nspace = Uniproc::State_space<dtime_t>::explore_naively(
+	                   jobs, 0, 20, dag, 1, 0, false);
+	CHECK_FALSE(nspace.is_schedulable());
+
+	auto space = Uniproc::State_space<dtime_t>::explore(
+		                jobs, 0, 20, dag, 1, 0, false);
+	CHECK_FALSE(space.is_schedulable());
+}
+
+const std::string deadend_jobs_file =
+"   Task ID,     Job ID,          Arrival min,          Arrival max,             Cost min,             Cost max,             Deadline,             Priority\n"
+"1, 1,  0,  0, 1,  2, 10, 10\n"
+"1, 2, 10, 10, 1,  2, 20, 20\n"
+"1, 3, 20, 20, 1,  2, 30, 30\n"
+"1, 4, 30, 30, 1,  2, 40, 40\n"
+"1, 5, 40, 40, 1,  2, 50, 50\n"
+"1, 6, 50, 50, 1,  2, 60, 60\n";
+
+
+const std::string deadend_dag_file =
+"Predecessor TID,	Predecessor JID,	Successor TID, Successor JID\n"
+"1, 1,    1, 2\n"
+"1, 2,    1, 3\n"
+"1, 3,    1, 4\n"
+"1, 4,    1, 5\n"
+"1, 5,    1, 6\n"
+"1, 6,    1, 1\n";  // back edge
+
+
+TEST_CASE("[prec] handle analysis deadend gracefully") {
+	auto dag_in = std::istringstream(deadend_dag_file);
+	auto dag = NP::parse_dag_file(dag_in);
+
+	auto in = std::istringstream(deadend_jobs_file);
+	auto jobs = NP::parse_file<dtime_t>(in);
+
+	NP::validate_prec_refs<dtime_t>(dag, jobs);
+
+	auto nspace = Uniproc::State_space<dtime_t>::explore_naively(
+	                   jobs, 0, 20, dag, 1, 0, false);
+	CHECK_FALSE(nspace.is_schedulable());
+
+	auto space = Uniproc::State_space<dtime_t>::explore(
+		                jobs, 0, 20, dag, 1, 0, false);
+	CHECK_FALSE(space.is_schedulable());
+}
+
