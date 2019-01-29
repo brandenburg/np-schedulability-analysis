26149b5d72f2667db40160b263c6a8d310d6b9b4
Björn Brandenburg <bbb@mpi-sws.org>
2019-01-29 14:24:03 +0100

    add class for tracking abort actions

---
+++ include/aborts.hpp
@@ -0,0 +1,55 @@
+#ifndef ABORT_HANDLER_HPP
+#define ABORT_HANDLER_HPP
+
+#include "jobs.hpp"
+
+namespace NP {
+
+	template<class Time>
+	class Abort_action {
+
+	public:
+
+		Abort_action(JobID id,
+		             Interval<Time> trigger_time,
+		             Interval<Time> cleanup_cost)
+		: job_id(id)
+		, trigger_time(trigger_time)
+		, cleanup_cost(cleanup_cost)
+		{
+		}
+
+		JobID get_id() const
+		{
+			return job_id;
+		}
+
+		Time earliest_trigger_time() const
+		{
+			return trigger_time.from();
+		}
+
+		Time latest_trigger_time() const
+		{
+			return trigger_time.until();
+		}
+
+		Time least_cleanup_cost() const
+		{
+			return cleanup_cost.min();
+		}
+
+		Time maximum_cleanup_cost() const
+		{
+			return cleanup_cost.max();
+		}
+
+	private:
+		JobID job_id;
+		Interval<Time> trigger_time;
+		Interval<Time> cleanup_cost;
+	};
+
+}
+
+#endif
